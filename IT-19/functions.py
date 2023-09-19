a5349db4d7b8dd626f33fa57065905ebb70b4a9e
Sajo8093 <sandra.jonsson.809>
2023-09-19 20:00:20 +0200

    added functions

---
+++ IT-19/functions.py
@@ -0,0 +1,42 @@
+a=3
+b=14
+
+n=0
+m=0
+    
+def double(n):
+    return 2*n
+        
+def triple(n):
+    return 3*n
+
+def quadruple(n):
+    return double(n)*2
+
+def funky(n,m):
+    return triple(n)+ quadruple(m)
+
+d1=double(a)
+d2=double(b)
+
+t1=triple(a)
+t2=triple(b)
+
+q1=quadruple(a)
+q2=quadruple(b)
+
+f1=funky(a,b)
+f2=funky(b,b)
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
