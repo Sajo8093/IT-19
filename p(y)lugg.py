dbe54adb13757d147e5ea709f6b23a2538d98fdd
Moot <60465687+ertkonsert@users.noreply.github.com>
2023-10-16 15:07:16 +0200

    Add basic menu and swedish words

--- p(y)lugg.py
+++ p(y)lugg.py
@@ -20,21 +20,57 @@ def menu(title, prompt, options):
     
     return action
 
-def swedish(difficulty):
+def list_to_dict(ls):
+    x = 1
+    ds = {}
+    for item in ls:
+        ds[str(x)] = item
+        x = x + 1
+    return ds
+
+#def shuffle_dict():
     
+
+def swedish(options):
+    swedish_words = ["förespråka", "obefogad", "emballage"]
+    swedish_words_correct = {"förespråka":"rekommendera", "obefogad":"grundlös"}
+    swedish_words_alternatives = {"förespråka":["reflektera", "reservera", "rekommendera", "resonera", "regissera"], "obefogad":["grundlös", "maktlös", "ansvarslös", "gränslös", "meningslös"]}
+    
+    action = ""
+    while action != "2":
+        points = 0
+        for word in swedish_words_alternatives:
+            print()
+            answer = menu(word, "Val: ", list_to_dict(swedish_words_alternatives[word]))
+            if swedish_words_alternatives[word][int(answer) - 1] == swedish_words_correct[word]:
+                print()
+                print("Rätt!")
+                points = points + 1
+            else:
+                print()
+                print("Fel.")
+        print()
+        print(f"Du fick {points} antal rätt!")
+        print()
+        action = menu("Vad vill du göra?", "Val: ", options)
+    
+#def words(words):
+    
+
+
 
 def main():
     options_main = {"1":"Svenska ord", "2":"Engelska-Svenska", "3":"Avsluta"}
-    options_difficulty = {"1":"Lätt", "2":"Svårt"}
+    options_difficulty = {"1":"Lätt", "2":"Medel", "3":"Svårt"}
+    options_again = {"1":"Kör igen", "2":"Tillbaka till meny"}
     
     
     action = ""
     while action != "3":
+        splash()
         action = menu("Vad vill du p(y)lugga på?", "Val: ", options_main)
-        difficulty = menu("Välj svårighetsgrad:", "Val: ", options_difficulty)
-        if action == "l":
+        #difficulty = menu("Välj svårighetsgrad:", "Val: ", options_difficulty)
+        if action == "1":
+            swedish(options_again)
             
-
-                
-                
-                
+main()
