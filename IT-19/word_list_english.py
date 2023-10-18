27d4ef533331590bac378d131aee932807bff6b2
Sajo8093 <144773915+Sajo8093@users.noreply.github.com>
2023-10-18 14:11:16 +0200

    added list of words in english

---
+++ IT-19/word_list_english.py
@@ -0,0 +1,21 @@
+word_list = [
+    # Easy words
+    'cat', 'dog', 'run', 'jump', 'blue', 'red', 'cup', 'book', 'tree', 'fish',
+    'bird', 'happy', 'sad', 'hot', 'cold', 'big', 'small', 'good', 'bad', 'fast',
+    'slow', 'day', 'night', 'sun', 'moon', 'rain', 'snow', 'star', 'love', 'hate',
+    
+    # Moderate words
+    'benevolent', 'eclectic', 'enhance', 'innovate', 'obscure', 'ponder', 'rejuvenate', 'stagnant', 'tentative', 'vulnerable',
+    'accustomed', 'bewilder', 'complacent', 'debilitate', 'elusive', 'frugal', 'haphazard', 'imminent', 'manifest', 'nonchalant',
+    'obliterate', 'perceptive', 'quandary', 'resilient', 'scrutinize', 'tangible', 'undermine', 'vindicate', 'wary', 'zealous',
+    
+    # Difficult words
+    'alacrity', 'bucolic', 'convivial', 'ebullient', 'garrulous', 'indefatigable', 'jubilant', 'loquacious', 'mellifluous', 'nadir',
+    'obstreperous', 'panacea', 'quixotic', 'resplendent', 'serendipity', 'taciturn', 'ubiquitous', 'vacillate', 'wistful', 'xenophobe',
+    'yammer', 'zenith', 'abrogate', 'belligerent', 'cogent', 'deleterious', 'effervescent', 'fervent', 'gregarious', 'hedonist',
+    
+    # Very Difficult words
+    'abstemious', 'boondoggle', 'circumlocution', 'demagogue', 'egregious', 'facetious', 'gregarious', 'hierarchical', 'insidious', 'jejune',
+    'kowtow', 'lackadaisical', 'malinger', 'nepotism', 'obfuscate', 'paradigm', 'quagmire', 'recalcitrant', 'sanguine', 'taciturn',
+    'ubiquitous', 'vacuous', 'winsome', 'xenophobia', 'yearn', 'zeitgeist'
+]
