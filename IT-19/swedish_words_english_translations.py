49ccea23eca318b65cdb5c533cf3f56ae7f7dcb2
Sajo8093 <144773915+Sajo8093@users.noreply.github.com>
2023-10-18 14:19:26 +0200

    list of swedish words

---
+++ IT-19/swedish_words_english_translations.py
@@ -0,0 +1,15 @@
+values = [
+    "bil", "hund", "katt", "hus", "bord", "dator", "telefon", "träd", "glad", "ledsen",
+    "springa", "gå", "hoppa", "skratta", "gråta", "simma", "flyga", "fågel", "blomma",
+    "flod", "berg", "strand", "hav", "bok", "penna", "blyertspenna", "skola", "elev",
+    "lärare", "läkare", "sjuksköterska", "tandläkare", "advokat", "polis", "eld", "sol",
+    "måne", "stjärna", "himmel", "moln", "regn", "snö", "vind", "varm", "kall", "stor",
+    "liten", "snabb", "långsam", "hög", "låg", "svår", "lätt", "dyr", "billig", "ren",
+    "smutsig", "ljus", "mörk", "god", "smaklös", "tyst", "bullrig", "upptagen", "lugn",
+    "arg", "snäll", "elak", "mjuk", "hård", "gammal", "ny", "modern", "forntida", "modig",
+    "fegis", "ärlig", "ohederlig", "vis", "dum", "vän", "fiende", "kärlek", "hat", "fred",
+    "krig", "framgång", "misslyckande", "hälsa", "sjukdom", "hungrig", "mätt", "törstig",
+    "torr"
+]
+
+
